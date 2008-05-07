35f49e101c96cd67e5a0c7114b703402157c2611
Seth Croston Barber <seth@0kelvin.net>
2008-05-06 23:17:34 -0700

    Added some properties to poems for ratings and favourites.

--- src/badpoetry/badpoetry/model/__init__.py
+++ src/badpoetry/badpoetry/model/__init__.py
@@ -2,11 +2,15 @@ from google.appengine.ext import db
 
 class Poem(db.Model):
 	author = db.UserProperty()
-	title = db.StringProperty()
-	content = db.TextProperty()
+	title = db.StringProperty(unicode)
+	content = db.TextProperty(unicode)
 	tags = db.ListProperty(unicode)
+	rating = db.FloatProperty() # 3.4 stars
+	total_ratings = db.IntegerProperty() # 17
+	number_of_ratings = db.IntegerProperty() # 5
+	favorited = db.IntegerProperty() # 3 (3 people marked this as a favourite)
 	created = db.DateTimeProperty(auto_now_add=True)
 
 class Tag(db.Model):
-	tag = db.StringProperty()
+	tag = db.StringProperty(unicode)
 	count = db.IntegerProperty()
