b1fda9765d2735cb6731793f48b2f1172a717746
Seth Croston Barber <seth@0kelvin.net>
2008-05-06 22:04:24 -0700

    Added an "all our tags" list.

--- src/badpoetry/badpoetry/controllers/tags.py
+++ src/badpoetry/badpoetry/controllers/tags.py
@@ -5,6 +5,13 @@ from badpoetry.lib.base import *
 log = logging.getLogger(__name__)
 
 class TagsController(BaseController):
+	def __init__(self):
+		g.tags = model.Tag().all().order('-count').order('tag').fetch(limit=10)
+	
+	def index(self):
+		c.tags = model.Tag.all().order('-count').order('tag')
+		return render('/tags/index.mako')
+	
 	def show(self, id):
 		#query = db.Query(model.Poem)
 		c.poems = model.Poem.all().filter("tags = ", id)
